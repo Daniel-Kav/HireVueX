67dc93f69a5267ac8828ed14c280aed02672a1da
daniel-kav <dankavatha01@gmail.com>
2025-02-07 11:39:50 +0300

    Enhance Navbar component with updated logo and improved styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,33 @@
-import Link from "next/link";
-import { ModeToggle } from "./ModeToggle";
-import { CodeIcon } from "lucide-react";
-import { SignedIn, UserButton } from "@clerk/nextjs";
-import DasboardBtn from "./DasboardBtn";
+import Link from "next/link"
+import { ModeToggle } from "./ModeToggle"
+import { SignedIn, UserButton } from "@clerk/nextjs"
+import DasboardBtn from "./DasboardBtn"
 
 function Navbar() {
   return (
-    <nav className="border-b">
+    <nav className="border-b border-gray-800 bg-gray-900">
       <div className="flex h-16 items-center px-4 container mx-auto">
         {/* LEFT SIDE -LOGO */}
         <Link
           href="/"
           className="flex items-center gap-2 font-semibold text-2xl mr-6 font-mono hover:opacity-80 transition-opacity"
         >
-          <CodeIcon className="size-8 text-orange-500" />
-          <span className="bg-gradient-to-r from-orange-500 to-emerald-500 bg-clip-text text-transparent animate-gradient">
-          HireVueX
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-8 h-8 text-blue-500"
+          >
+            <path
+              strokeLinecap="round"
+              d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
+            />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
+          </svg>
+          <span className="bg-gradient-to-r from-blue-500 to-blue-300 bg-clip-text text-transparent animate-gradient">
+            HireVueX
           </span>
         </Link>
 
@@ -29,6 +41,8 @@ function Navbar() {
         </SignedIn>
       </div>
     </nav>
-  );
+  )
 }
-export default Navbar;
+
+export default Navbar
+
